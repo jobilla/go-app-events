42a0b36b6a371b7f448132a237c9ed116b0ee058
Vladimir Andronov <vladimir.andronov@gmail.com>
2020-01-02 15:45:35 +0200

    implement listener using cloud.google.com/go/pubsub

--- pkg/appevents/listener.go
+++ pkg/appevents/listener.go
@@ -1,9 +1,10 @@
 package appevents
 
 import (
+	"cloud.google.com/go/pubsub"
 	"context"
 	"encoding/json"
-	"gocloud.dev/pubsub"
+	"fmt"
 )
 
 type AppEventHandler func(payload []byte, event string)
@@ -13,13 +14,31 @@ type ProtoBody struct {
 }
 
 func (p *ProtoBody) FromPubsubMessage(message *pubsub.Message) error {
-	return json.Unmarshal(message.Body, p)
+	return json.Unmarshal(message.Data, p)
 }
 
 type Listener struct {
 	subscription *pubsub.Subscription
 	ctx          context.Context
 	handlers     map[string]AppEventHandler
+}
+
+func (l *Listener) Bootstrap2() {
+	l.ctx = context.Background()
+}
+
+func (l *Listener) Bootstrap(projectID string, subID string) error {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+
+	subscription := client.Subscription(subID)
+	l.subscription = subscription
+	l.ctx = context.Background()
+
+	return err
 }
 
 func (l *Listener) RegisterHandlers(handlers map[string]AppEventHandler) {
@@ -34,23 +53,19 @@ func (l *Listener) RegisterHandlers(handlers map[string]AppEventHandler) {
 
 func (l *Listener) Listen() error {
 	for {
-		message, err := l.subscription.Receive(l.ctx)
+		err := l.subscription.Receive(l.ctx, func (ctx context.Context, message *pubsub.Message) {
+			if handler, ok := l.handlers[message.Attributes["event"]]; ok {
+				body := &ProtoBody{}
+				body.FromPubsubMessage(message)
+
+				handler(body.Payload, message.Attributes["event"])
+			}
+
+			message.Ack()
+		})
 
 		if err != nil {
 			return err
 		}
-
-		if handler, ok := l.handlers[message.Metadata["event"]]; ok {
-			body := &ProtoBody{}
-			err := body.FromPubsubMessage(message)
-
-			if err != nil {
-				return err
-			}
-
-			handler(body.Payload, message.Metadata["event"])
-		}
-
-		message.Ack()
 	}
 }
